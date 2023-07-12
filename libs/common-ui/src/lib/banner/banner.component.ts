d550c39b15b6a110d1c1ad2c26413b486f0087e0
esky <eugene.sky.dev@gmail.com>
2023-07-12 14:02:11 +0300

    generated BannerComponent in common-ui lib

---
+++ libs/common-ui/src/lib/banner/banner.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sky-devs-banner',
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.scss'],
+})
+export class BannerComponent {}
